474b3e5f84b2ef34577219e918aeb78b2de878a5
WesleyPodolsky <podolskywesley@gmail.com>
2024-02-11 12:50:20 -0800

    Revert "sky and graas"
    This reverts commit fb301ba7

--- _01_drawing/__init__.py
+++ _01_drawing/__init__.py
@@ -1,8 +1,5 @@
 import turtle
 
-if __name__ == '__main__':
-    window = turtle.Screen()
-    window.bgcolor('lightblue')
 
 my_turtle = turtle.Turtle()
 my_turtle = turtle.penup()
@@ -28,15 +25,6 @@ my_turtle = turtle.left(45)
 my_turtle = turtle.color('brown')
 my_turtle = turtle.forward(-40)
 my_turtle = turtle.left(45)
-my_turtle = turtle.left(-180)
-my_turtle = turtle.forward(35)
-my_turtle = turtle.stamp()
-my_turtle = turtle.forward(-20)
-my_turtle = turtle.stamp()
-my_turtle = turtle.shapesize(70)
-my_turtle = turtle.color("green")
-my_turtle = turtle.forward(730)
-
 
 
 
